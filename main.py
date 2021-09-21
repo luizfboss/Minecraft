2e86700084cc3ad3938793b863458b574a9b3343
luizfboss <luiz.fbossetto@outlook.com>
2021-09-20 22:01:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -20,12 +20,11 @@ WALKING_SPEED = 5
 FLYING_SPEED = 15
 
 GRAVITY = 10.0
-MAX_JUMP_HEIGHT = 1.0  # About the height of a block.
-# To derive the formula for calculating jump speed, first solve
-#    v_t = v_0 + a * t
-# for the time at which you achieve maximum height, where a is the acceleration
-# due to gravity and v_t = 0. This gives:
-#    t = - v_0 / a
+MAX_JUMP_HEIGHT = 1.0  # Altura do bloco.
+# Velocidade do pulo (v_t = v_0 + a * t) para altura maxima
+# ** a = aceleracao
+
+# Para gravidade v_t = 0 ||| t = - v_0 / a
 # Use t and the desired MAX_JUMP_HEIGHT to solve for v_0 (jump speed) in
 #    s = s_0 + v_0 * t + (a * t^2) / 2
 JUMP_SPEED = math.sqrt(2 * GRAVITY * MAX_JUMP_HEIGHT)
